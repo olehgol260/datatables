c522d72afacd97404ad42e6b3d6fadb5ea8c1f87
olehgol260 <olehgol260@gmail.com>
2016-08-16 23:40:27 +0300

    Add error checks while parsing data from url value

--- datatables.go
+++ datatables.go
@@ -4,41 +4,59 @@ import (
 	"github.com/astaxie/beego/orm"
 	"net/url"
 	"strconv"
+	"errors"
 )
 
-var RegisterColumns map[string]interface{} = map[string]interface{}{
-
-}
+var registerColumns map[string]interface{} = map[string]interface{}{}
 
 type Data struct {
-	Ctx		url.Values //get args
-	DBName		string	//DB name
-	TableName   	string	//table name
-	Columns		[]string //select column
-	SearchFilter	[]string //where filter
-	Model           interface{}
+	Ctx          url.Values //get args
+	DBName       string     //DB name
+	TableName    string     //table name
+	Columns      []string   //select column
+	SearchFilter []string   //where filter
+	Model        interface{}
 }
 
+func (p *Data)Table() (rs interface{}, err error) {
+	start, err := strconv.Atoi(p.Ctx.Get("start"))
+	if err != nil {
+		return nil, errors.New("invalid url parameter: start must be a valid integer")
+	}
 
+	length, err := strconv.Atoi(p.Ctx.Get("length"))
+	if err != nil {
+		return nil, errors.New("invalid url parameter: length must be a valid integer")
+	}
 
-func (p *Data)Table() (rs interface{}, err error){
-	start,err := strconv.Atoi(p.Ctx.Get("start"))
-	length,err := strconv.Atoi(p.Ctx.Get("length"))
 	search := p.Ctx.Get("search[value]")
-	order_column,err := strconv.Atoi(p.Ctx.Get("order[0][column]"))
-	order_dir := p.Ctx.Get("order[0][dir]")
-	draws,err := strconv.Atoi(p.Ctx.Get("draw"))
 
+	order_column, err := strconv.Atoi(p.Ctx.Get("order[0][column]"))
+	if err != nil {
+		return nil, errors.New("invalid url parameter: order[0][column] must be a valid integer")
+	}
+
+	order_dir := p.Ctx.Get("order[0][dir]")
+	if order_dir != "asc" || order_dir != "desc" {
+		return nil, errors.New("invalid url parameter: order[0][dir] may be either 'asc' or 'desc'")
+	}
+
+	draws, err := strconv.Atoi(p.Ctx.Get("draw"))
+	if err != nil {
+		return nil, errors.New("invalid url parameter: draw must be a valid integer")
+	}
 
 	//query field
 	var selectStr string
-	for k,v := range p.Columns{
-		if k != 0{ selectStr += ","}
+	for k, v := range p.Columns {
+		if k != 0 {
+			selectStr += ","
+		}
 		selectStr += v
 	}
 
 	//search
-	var whereStr  string
+	var whereStr string
 	search_len := len(search)
 
 	//offset
@@ -46,21 +64,22 @@ func (p *Data)Table() (rs interface{}, err error){
 	qb, _ := orm.NewQueryBuilder("mysql")
 
 	//search
-	if search_len >0 {
-		for k,v := range p.SearchFilter{
+	if search_len > 0 {
+		for k, v := range p.SearchFilter {
 			if k != 0 {
-				whereStr += " OR "}
-			whereStr +=  v + " LIKE " + "\"%"+search+"%\"" //like
+				whereStr += " OR "
+			}
+			whereStr += v + " LIKE " + "\"%" + search + "%\"" //like
 		}
-		if order_dir == "asc"{
+		if order_dir == "asc" {
 			qb.Select(selectStr).From(p.TableName).Where(whereStr).OrderBy(p.Columns[order_column]).Asc().Limit(length).Offset(colOffset)
-		}else{
+		} else {
 			qb.Select(selectStr).From(p.TableName).Where(whereStr).OrderBy(p.Columns[order_column]).Desc().Limit(length).Offset(colOffset)
 		}
-	}else{
-		if order_dir == "asc"{
+	} else {
+		if order_dir == "asc" {
 			qb.Select(selectStr).From(p.TableName).OrderBy(p.Columns[order_column]).Asc().Limit(length).Offset(colOffset)
-		}else{
+		} else {
 			qb.Select(selectStr).From(p.TableName).OrderBy(p.Columns[order_column]).Desc().Limit(length).Offset(colOffset)
 		}
 	}
@@ -68,24 +87,22 @@ func (p *Data)Table() (rs interface{}, err error){
 	o := orm.NewOrm()
 	o.Using(p.DBName)
 
-	cl := RegisterColumns[p.TableName]
+	cl := registerColumns[p.TableName]
 	num, err := o.Raw(sql).QueryRows(cl)
-
-
 
 	recordTotal, err := o.QueryTable(p.TableName).Count() //data sum
 	var recordsFiltered int32 //search data sum
 	type rFilterCont struct {
-		CntNumber 		int
+		CntNumber int
 	}
 	var rcount rFilterCont
-	if search_len >0 {
+	if search_len > 0 {
 		qb2, _ := orm.NewQueryBuilder("mysql")
 		qb2.Select("Count(*) AS cnt_number ").From(p.TableName).Where(whereStr)
 		sqlFilter := qb2.String()
 		o.Raw(sqlFilter).QueryRow(&rcount)
 		recordsFiltered = int32(rcount.CntNumber)
-	}else{
+	} else {
 		recordsFiltered = int32(recordTotal)
 	}
 
@@ -94,12 +111,13 @@ func (p *Data)Table() (rs interface{}, err error){
 		"recordsTotal": recordTotal,
 		"recordsFiltered": recordsFiltered,
 		"data":  func() (interface{}) {
-		if num == 0{
-			rest := []string {}
-			return rest
-		}else{
-			return cl
-		}}()},
-	err
+			if num == 0 {
+				rest := []string{}
+				return rest
+			} else {
+				return cl
+			}
+		}()},
+		err
 
 }
